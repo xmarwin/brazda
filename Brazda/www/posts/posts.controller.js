0dccb76e78ce93d3fa7235889dfcdd2bb010cb8c
Martin Slezák <martin@slezakovi.com>
2017-07-02 22:41:36 +0200

    Upravy seznamu stanovist

--- Brazda/www/posts/posts.controller.js
+++ Brazda/www/posts/posts.controller.js
@@ -11,19 +11,41 @@ angular.module('myApp.posts', ['ngRoute'])
 
     .controller('PostsCtrl', PostsController);
 
-PostsController.$inject = ['AuthService', 'PostService'];
+PostsController.$inject = ['AuthService', 'PostService', '$filter'];
 
-function PostsController(authService, postService) {
+function PostsController(authService, postService, $filter) {
     var vm = this;
 
+    vm.postTypes = [];
+    vm.filter = [];
+    vm.showFilter = false;
+
     var init = function () {
-        vm.posts = postService.getPosts(authService.team.id);
+        loadPosts();
+
+        vm.postTypes = postService.getPostTypes();
+        vm.postColors = postService.getPostColors();
     }
 
+    vm.filter = function () {
+        var postTypeFilter = (angular.isUndefined(vm.filter.post_type) || vm.filter.post_type == null) ? "" : angular.isUndefined(vm.filter.post_type.post_type) ? "" : vm.filter.post_type.post_type;
+        var postColorFilter = (angular.isUndefined(vm.filter.color) || vm.filter.color == null) ? "" : angular.isUndefined(vm.filter.color.color) ? "" : vm.filter.color.color;
 
+        vm.postsFiltered = $filter('filter')(vm.posts, { "post_type": postTypeFilter, "color": postColorFilter });
+    }
 
+    vm.toggleFilter = function() {
+        vm.showFilter = !vm.showFilter;
+    }
 
+    vm.refresh = function () {
+        loadPosts();
+    }
 
+    var loadPosts = function () {
+        vm.posts = postService.getPosts(authService.team.id);
+        vm.filter();
+    }
 
     init();
 }
