ffd90e2df88c62e3b2cbbf4fa4b0a8ec4157f589
Tomáš Hofman <hofman.tomas@gmail.com>
2017-08-31 16:05:14 +0200

    Zapnutí debug modu pro ip 79.127.215.114.

--- Brazda/app/bootstrap.php
+++ Brazda/app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->setDebugMode('79.127.215.114'); // enable for your remote IP
 $configurator->enableTracy(realpath(__DIR__ . '/../log'));
 
 $configurator->setTimeZone('Europe/Prague');
