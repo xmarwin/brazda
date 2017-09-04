fedb03b52696096d964ee8508a75f186f7f91fc7
Tomáš Hofman <hofman.tomas@gmail.com>
2017-09-04 20:05:38 +0200

    Doplnění metody pro přípravu výpočtu výsledků.

---
+++ Brazda/app/presenters/ResultPresenter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Brazda\Presenters;
+
+use Nette\Security,
+    Brazda\Models,
+    Drahak\Restful\Validation;
+
+class ResultPresenter extends SecuredBasePresenter
+{
+    protected
+        $logs,
+        $teams;
+
+    public function startup()
+    {
+        parent::startup();
+
+        $this->logs  = $this->context->getService('logs');
+        $this->teams = $this->context->getService('teams');
+    } // startup()
+
+    public function actionList()
+    {
+        $this->checkAdministrator();
+
+        $teams = [];
+        $teamsData = (array) $this->teams->view(['t.team_type' => 'COM']);
+        foreach ($teamsData as $teamItem) {
+            $team = [
+                'team' => $teamItem->team,
+                'name' => $teamItem->name,
+                'logs' => []
+            ];
+//dump($this->logs->view(['team' => $teamItem->team]));exit;
+            $team['logs'] = $this->logs->resultView([
+                'team' => $teamItem->team,
+            ])->fetchAll();
+
+            $teams[] = $team;
+        } // foreach
+
+        $this->resource = $teams;
+        $this->sendResource($this->outputType);
+    } // actionList()
+} // ResultPresenter
