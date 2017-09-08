4ae04a8a2f3ef05a288bf2d9d8db270fab3037a5
Tomáš Hofman <hofman.tomas@gmail.com>
2017-09-08 12:25:08 +0200

    Příprava testovací větve s loňskými daty.

--- Brazda/db/db-reset.bat
+++ Brazda/db/db-reset.bat
@@ -6,73 +6,74 @@ chcp 65001
 SET PGCLIENTENCODING=UTF-8
 SET PGUSER=postgres
 SET PGPASSWORD=
-SET PGDATABASE=brazda
+SET PGDATABASE=brazda_test
 
 ECHO Ruším starou databázi BRAZDA...
 "C:\Program Files\PostgreSQL\9.5\bin\dropdb.exe" brazda
-if errorLevel 1 (
-    exit /b %errorLevel%;
-)
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Vytvářím novou databázi BRAZDA...
 "C:\Program Files\PostgreSQL\9.5\bin\createdb.exe" -O brazda -E UTF8 -T template0
-if errorLevel 1 (
-    exit /b %errorLevel%;
-)
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 SET PGUSER=brazda
 SET PGPASSWORD=FzwtMS/jq.XSQ
-SET PGDATABASE=brazda
+SET PGDATABASE=brazda_test
 
 ECHO Importuji schéma...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f schema.sql
-if errorLevel 1 (
-    exit /b %errorLevel%;
-)
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Importuji základní data...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f data.sql
-if errorLevel 1 (
-    exit /b %errorLevel%;
-)
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 001...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p001.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 002...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p002.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 003...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p003.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 004...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p004.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 005...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p005.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 006...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p006.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 007...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p007.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 008...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p008.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 009...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p009.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 009...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p010.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Importuji data o stanovištích...
 "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f posts.sql
-if errorLevel 1 (
-    exit /b %errorLevel%;
-)
+if errorLevel 1 ( exit /b %errorLevel%; )
 
-REM ECHO Vytvářím pohled na výsledky...
-REM "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f results.sql
+ECHO Importuji data z posledního závodu...
+"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f results.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Hotovo!
