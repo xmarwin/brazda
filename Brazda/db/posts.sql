600374c9fd7830e9c227722568caefdef096d5eb
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2017-09-12 18:20:17 +0200

    Úprava dat stanovišť.

--- Brazda/db/posts.sql
+++ Brazda/db/posts.sql
@@ -16,7 +16,7 @@ INSERT INTO teams (team, team_type, name, shibboleth, description) VALUES
 	(13, 'COM','Čuňoši', 'Kolaloka?', '');
 --	(14, 'COM','(ještě se poradíme)', 'nevímdál.', '');
 
-SELECT setval('teams_team_seq', 13);
+SELECT setval('teams_team_seq', 13) AS set_teams_team_seq;
 
 ---------------------------------------------
 
@@ -251,7 +251,7 @@ INSERT INTO posts (post, post_type,color,name,max_score,difficulty,terrain,cache
             (32, 'END', 'TRA', 'Cíl', 0, 1, 1, 'O', null, 'Finito', True, '', '<p><strong>Gratulejeme k dokončení závodu BRAZDA 2016</strong>.<br> Těšíme se na Vás u vyhlášení výsledků.</p>
                 <p><strong>Uvidíme se zde</strong> opět <strong>v 18.30</strong>, kdy závod končí.</p>', null, null, 49.4671494, 18.1633683);
 
-SELECT setval('posts_post_seq', 32);
+SELECT setval('posts_post_seq', 32) AS set_posts_post_seq;
 
 
 -------------------------------------------
