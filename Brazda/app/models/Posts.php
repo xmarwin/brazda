7785169bf75df4775f4ffed26379c5a3a7b2710a
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2018-06-14 22:00:18 +0200

    Oprava výpisu detailu stanoviště.

--- Brazda/app/models/Posts.php
+++ Brazda/app/models/Posts.php
@@ -59,7 +59,7 @@ class Posts extends Base
         )->fetch();
     } // find()
 
-public function listView(array $filter = [], array $order = [ 'pt.rank' => 'ASC', 'p.color' => 'ASC', 'p.name' => 'ASC' ], array $limit = [])
+	public function listView(array $filter = [], array $order = [ 'pt.rank' => 'ASC', 'p.color' => 'ASC', 'p.name' => 'ASC' ], array $limit = [])
     {
         $filter = $this->normalizeFilter($filter, [
             'post'       => 'p.post',
@@ -200,16 +200,16 @@ public function listView(array $filter = [], array $order = [ 'pt.rank' => 'ASC'
                 p.difficulty,
                 p.terrain,
                 p.cache_size,
-                -- p.description,
+                p.description,
                 p.cache_type,
                 p.max_score,
-                -- p.with_staff,
-                -- p.latitude,
-                -- p.longitude,
+                p.with_staff,
+                p.latitude,
+                p.longitude,
                 p.hint,
-                -- to_char(p.open_from, 'HH24:MI') AS open_from,
-                -- to_char(p.open_to, 'HH24:MI') AS open_to,
-                -- CASE WHEN p.help NOT LIKE '' THEN TRUE ELSE FALSE END AS has_help,
+                to_char(p.open_from, 'HH24:MI') AS open_from,
+                to_char(p.open_to, 'HH24:MI') AS open_to,
+                CASE WHEN p.help NOT LIKE '' THEN TRUE ELSE FALSE END AS has_help,
 
                 cs.name AS size_name,
                 ct.name AS cache_name,
@@ -220,7 +220,7 @@ public function listView(array $filter = [], array $order = [ 'pt.rank' => 'ASC'
                 pt.name AS type_name,
                 pt.rank AS type_rank,
 
-                -- coalesce(pn.note, NULL) AS post_note,
+                coalesce(pn.note, NULL) AS post_note,
 
             %if", isset($team) && $role == Teams::COMPETITORS, "
                 CASE WHEN lo.moment IS NOT NULL THEN p.shibboleth ELSE NULL END AS shibboleth,
