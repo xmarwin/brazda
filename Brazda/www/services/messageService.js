31873557decde2d8f6e4c3513fa1240e5d0d5805
Martin Slezák <martin@slezakovi.com>
2018-07-30 21:43:59 +0200

    Přidáno vytváření zpráv

---
+++ Brazda/www/services/messageService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+angular.module('myApp.messageService', [])
+    .service('MessageService', MessageService)
+
+MessageService.$inject = ['$interval', 'WebApiService', 'localStorageService', '$rootScope'];
+
+function MessageService($interval, webApiService, localStorageService, $rootScope) {
+    var vm = this;
+
+    vm.createMessage = function (data) {
+        return webApiService.get('message/create', data, 'POST');
+    }
+
+    vm.getAllMessages = function (data) {
+        return webApiService.get('message/all', data, 'GET');
+    }
+}
