5c1208ac58871570099929d662e9dc1ef3675665
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2018-08-14 16:24:00 +0200

    Oprava problému s nezobrazováním bonusového hesla.

--- Brazda/db/test/p015.sql
+++ Brazda/db/test/p015.sql
@@ -4,4 +4,7 @@ create table "schema" (
 	"version"	integer	not null default 15
 );
 
+insert into "schema"
+values (15);
+
 commit;
--- Brazda/db/test/p016.sql
+++ Brazda/db/test/p016.sql
@@ -4,6 +4,6 @@ insert into post_colors
 values ('GLD', 'Zlatá', 'rgb(255,215,0)');
 
 update "schema"
-set version = 16;
+set "version" = 16;
 
 commit;
