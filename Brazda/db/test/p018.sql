0a255d82a451b7a80ada099f5e4652deb84dacff
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2018-08-14 18:40:23 +0200

    Úprava vracených hesel, doplnění dat pro testovaní nového systému hesel.

--- Brazda/db/test/p018.sql
+++ Brazda/db/test/p018.sql
@@ -13,7 +13,30 @@ as $$
         p.password_character
       from posts p
       where p.color like inColor
-        and p.password_position > 0
+        and p.post_type not in ('BON', 'SBN')
+      order by p.password_position
+    loop
+      password := concat(password, rec.password_character);
+    end loop;
+
+    return password;
+  end;
+$$ language plpgsql;
+
+
+create or replace function adminSuperbonusPassword() returns character varying (20)
+as $$
+  declare
+    password character varying (20) := null;
+    rec record;
+
+  begin
+    for rec in
+      select
+        p.password_position,
+        p.password_character
+      from posts p
+      where p.post_type like 'BON'
       order by p.password_position
     loop
       password := concat(password, rec.password_character);
