e166555916898ce22df2c83842aefdda643945e6
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2018-08-17 14:20:39 +0200

    Úprava databázových dat pro ostrou verzi 2018.

--- Brazda/db/master/db-reset.bat
+++ Brazda/db/master/db-reset.bat
@@ -1,78 +1,107 @@
-TITLE Nástroj na reset databáze závodu BRAZDA
-ECHO OFF
+﻿TITLE Nástroj na reset databáze závodu BRAZDA
+REM ECHO OFF
 
-chcp 65001
+REM chcp 65001
 
-SET PGCLIENTENCODING=UTF-8
+SET PGVERSION=9.5
+SET PGCLIENTENCODING=UTF8
 SET PGUSER=postgres
 SET PGPASSWORD=
 SET PGDATABASE=brazda
 
 ECHO Ruším starou databázi BRAZDA...
-"C:\Program Files\PostgreSQL\9.5\bin\dropdb.exe" brazda
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\dropdb.exe" brazda_test
 
 ECHO Vytvářím novou databázi BRAZDA...
-"C:\Program Files\PostgreSQL\9.5\bin\createdb.exe" -O brazda -E UTF8 -T template0
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\createdb.exe" -O brazda_test -E UTF8 -T template0  -U postgres -O brazda
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 SET PGUSER=brazda
 SET PGPASSWORD=FzwtMS/jq.XSQ
-SET PGDATABASE=brazda
+SET PGDATABASE=brazda_test
 
 ECHO Importuji schéma...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f schema.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f schema.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Importuji základní data...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f data.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f data.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 001...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p001.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p001.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 002...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p002.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p002.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 003...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p003.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p003.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 004...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p004.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p004.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 005...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p005.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p005.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 006...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p006.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p006.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 007...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p007.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p007.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 008...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p008.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p008.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 009...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p009.sql
-if errorLevel 1 ( exit /b %errorLevel%; )
-
-ECHO Aplikuji patch 010..
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p010.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p009.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Aplikuji patch 010...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f p011.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p010.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 011...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p011.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 012...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p012.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 013...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p013.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 014...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p014.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 015...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p015.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 016...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p016.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 017...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p017.sql
+if errorLevel 1 ( exit /b %errorLevel%; )
+
+ECHO Aplikuji patch 018...
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f p018.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Importuji data o stanovištích...
-"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -f posts.sql
+"C:\Program Files\PostgreSQL\%PGVERSION%\bin\psql.exe" -f posts.sql
 if errorLevel 1 ( exit /b %errorLevel%; )
 
 ECHO Hotovo!
