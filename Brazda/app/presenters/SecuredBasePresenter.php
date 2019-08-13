b93a6173fe981641880e8a2ac06566d64499ba4b
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2019-08-13 17:17:18 +0200

    Úprava způsobu kontroly uživatele

--- Brazda/app/presenters/SecuredBasePresenter.php
+++ Brazda/app/presenters/SecuredBasePresenter.php
@@ -11,18 +11,24 @@ class SecuredBasePresenter extends BasePresenter
 		$input,
 
 		$logins,
+		$teams,
 
 		$team;
 
     public function startup()
     {
-        parent::startup();
+		parent::startup();
 
-        $this->logins = $this->context->getService('logins');
+		$this->logins = $this->context->getService('logins');
+		$this->teams  = $this->context->getService('teams');
 
-        $this->checkSecurityToken();
-        $this->team = !empty($this->getUser()->getIdentity())
-			? $this->getUser()->getIdentity()->getData()
+		$this->checkSecurityToken();
+
+		$parameters = $this->getRequest()->getParameters();
+		$login      = (array) $this->logins->find([ 'security_token' => $parameters['securityToken'] ]);
+		$team       = $this->teams->find([ 'team' => $login['team'] ]);
+		$this->team = !empty($team)
+			? $team
 			: [];
 
 		$this->input = $this->getInput();
@@ -58,18 +64,18 @@ class SecuredBasePresenter extends BasePresenter
 		return true;
     } // checkSecurityToken()
 
-    public function checkAdministrator()
-    {
-        if (!$this->getUser()->isInRole('ORG')) {
-            $this->sendErrorResource(
-                new Security\AuthenticationException(
-                    'Nemáte oprávnění pro volání této metody.',
-                    401
-                ) // AuthenticationException()
-            ); // sendErrorResource()
-        } // if
+	public function checkAdministrator()
+	{
+		if ($this->team['role'] !== 'ORG') {
+			$this->sendErrorResource(
+				new Security\AuthenticationException(
+					'Nemáte oprávnění pro volání této metody.',
+					401
+				) // AuthenticationException()
+			); // sendErrorResource()
+		} // if
 
-        return true;
-    } // checkAdministrator()
+		return true;
+	} // checkAdministrator()
 
 } // SecuredBasePresenter
