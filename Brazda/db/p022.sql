a3cbae76e62fcbf54b3868c2eb478a85b59c5a1e
Tomáš Hofman <tomas.hofman@hobrasoft.cz>
2019-08-15 11:28:11 +0200

    Úprava názvů ikon atributů

--- Brazda/db/p022.sql
+++ Brazda/db/p022.sql
@@ -6,8 +6,8 @@ select
 	a.name_on,
 	a.name_off,
 	'/assets/images/attributes/' || a.icon || '.png' AS icon,
-	'/assets/images/attributes/' || a.icon || '-on.png' AS icon_on,
-	CASE WHEN a.status_count = 3 THEN '/assets/images/attributes/' || a.icon || '-off.png' ELSE NULL END AS icon_off,
+	'/assets/images/attributes/' || a.icon || '-yes.png' AS icon_on,
+	CASE WHEN a.status_count = 3 THEN '/assets/images/attributes/' || a.icon || '-no.png' ELSE NULL END AS icon_off,
 	a.status_count
 from attributes a;
 
